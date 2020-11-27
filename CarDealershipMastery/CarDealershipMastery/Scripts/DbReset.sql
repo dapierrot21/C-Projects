ab492273d1692fb2746ececfcd551ec4d537bad8
Dean Pierrot <dapierrot21@gmail.com>
2020-11-27 15:22:56 -0500

    a few bugs fixed: These fixes goes along with improving the stored procedures by adding UserId, changes in the Sprocs means changes in the DBReset file.

--- CarDealershipMastery/CarDealershipMastery/Scripts/DbReset.sql
+++ CarDealershipMastery/CarDealershipMastery/Scripts/DbReset.sql
@@ -25,9 +25,10 @@ BEGIN
 	DELETE FROM [Role];
 	DELETE FROM AspNetUsers WHERE id = '00000000-0000-0000-0000-000000000000';
 
+END
 	
 
-
+BEGIN
 
 	-- State --
 	SET IDENTITY_INSERT [State] ON;
@@ -73,7 +74,7 @@ BEGIN
 
 	-- Contact --
 	SET IDENTITY_INSERT Contact ON;
-
+	DBCC CHECKIDENT ('Contact', RESEED, 1)
 	INSERT INTO Contact(ContactId, [Name], Email, Phone, [Message])
 	VALUES(1, 'Dean Pierrot', 'dapierrot21@gmail.com', '111-111-1111', 'Will like to buy a brand new telsa!'),
 	(2, 'Nipsey Hussle', 'tmc@gmail.com', '222-222-2222', 'Let me buy the whole please!!!'),
@@ -93,20 +94,20 @@ BEGIN
 	-- Model -- 
 	SET IDENTITY_INSERT Model ON;
 	DBCC CHECKIDENT ('Model', RESEED, 1)
-	INSERT INTO Model(ModelId, ModelType)
-	VALUES(1, 'Dodge'),
-	(2, 'Ford'),
-	(3, 'Tesla');
+	INSERT INTO Model(UserId, ModelId, ModelType)
+	VALUES('00000000-0000-0000-0000-000000000000', 1, 'Dodge'),
+	('00000000-0000-0000-0000-000000000000', 2, 'Ford'),
+	('00000000-0000-0000-0000-000000000000', 3, 'Tesla');
 
 	SET IDENTITY_INSERT Model OFF;
 
 	-- Make --
 	SET IDENTITY_INSERT Make ON;
 	DBCC CHECKIDENT ('Make', RESEED, 1)
-	INSERT INTO Make(MakeId, MakeType)
-	VALUES(1, 'Ram'),
-	(2, 'F-150'),
-	(3, 'Model S');
+	INSERT INTO Make(UserId, MakeId, MakeType)
+	VALUES('00000000-0000-0000-0000-000000000000', 1, 'Ram'),
+	('00000000-0000-0000-0000-000000000000', 2, 'F-150'),
+	('00000000-0000-0000-0000-000000000000', 3, 'Model S');
 
 	SET IDENTITY_INSERT Make OFF;
 
@@ -129,6 +130,7 @@ BEGIN
 
 	-- Specials --
 	SET IDENTITY_INSERT Specials ON;
+	DBCC CHECKIDENT ('Specials', RESEED, 1)
 	INSERT INTO Specials(SpecialsId, Title, [Description])
 	VALUES(1, 'Spring Break Deals', '25% off any car for students'),
 	(2, '4th of July Deals', '15% off and SUVs for veterans'),
@@ -146,10 +148,17 @@ BEGIN
 
 	-- Car --
 	SET IDENTITY_INSERT Car ON;
+	DBCC CHECKIDENT ('Car', RESEED, 1)
 	INSERT INTO Car(CarId, UserID, MakeId, ModelId, TypeId, BodyStyleId, TransmissionId, ColorId, InteriorId, [Year],
 	Milage, VIN, MSRP, SalePrice, [Description], UploadedPicture, IsFeatured)
-	VALUES(1, '00000000-0000-0000-0000-000000000000', 1, 1, 1, 1, 1, 2, 2, '2020', 'New', 12345678920987465, 150.00, 135.00, 'Brand New Car', 'Image file path', 0),
-	(2, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', 150000984575673829, 150.00, 125.00,'Brand New Car', 'Image file path', 1);
+	VALUES (1, '00000000-0000-0000-0000-000000000000', 1, 1, 1, 1, 1, 2, 2, '2020', 'New', '1TD67UHY89IT5RE2D', 150.00, 135.00, 'Brand New Car', 'Image file path', 1),
+	(2, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1),
+	(3, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1),
+	(4, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1),
+	(5, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1),
+	(6, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1),
+	(7, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1),
+	(8, 'dp', 1, 1, 1, 1, 2, 2, 2, '2020', '15,000', '1TD67UHY89IT5RE2D', 150.00, 125.00, 'Brand New Car', 'Image file path', 1)
 
 	SET IDENTITY_INSERT Car OFF;
 
@@ -158,11 +167,10 @@ BEGIN
 	-- SalesInfo --
 	SET IDENTITY_INSERT SalesInfo ON;
 	DBCC CHECKIDENT ('SalesInfo', RESEED, 1)
-	INSERT INTO SalesInfo(SalesInfoId, UserId, [Name], Email, Street1, Street2, City, StateId, ZipCode, PurchasePrice,
+	INSERT INTO SalesInfo (SalesInfoId, UserId, CarId, [Name], Email, Street1, Street2, City, StateId, ZipCode, PurchasePrice,
 	PurchaseTypeId)
-	VALUES( 1, 'sa', 'Dean Pierrot', 'dapierrot21@gmail.com', 'Main Street Ave', 'N/A', 2, 2, 40299, 150000.00, 1
-	),
-	(2, 'dp', 'Kaden', 'SupercoolKid@gmail.com', 'King Street Ln', 'Main Street Ave', 2, 2, 40299, 200000.00, 2);
+	VALUES( 1, '00000000-0000-0000-0000-000000000000', 1, 'Dean Pierrot', 'dapierrot21@gmail.com', 'Main Street Ave', 'N/A', 'Louisville', 2, 40299, 150000.00, 1),
+	(2, 'dp', 3, 'Kaden', 'SupercoolKid@gmail.com', 'King Street Ln', 'Main Street Ave', 'Louisville', 2, 40299, 200000.00, 2);
 
 	SET IDENTITY_INSERT SalesInfo OFF;
 
